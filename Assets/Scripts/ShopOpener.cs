456f3a0ff3a8cb0e2f38892a48e2e72672ad3b98
JeremyBasso <jmb4433@gmail.com>
2020-06-19 17:18:33 -0500

    Toggle Shop Button

---
+++ Assets/Scripts/ShopOpener.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopOpener : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void OpenPanel()
+    {
+        if(Panel != null)
+        {
+            Panel.SetActive(true);
+        }
+        if(Panel == null)
+        {
+            Panel.SetActive(false);
+        }
+    }
+}
