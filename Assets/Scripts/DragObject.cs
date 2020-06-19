c9d26d49e37e1cb40c1210cc7afaa0ff0c5a5c3d
mhulcy <hulcy@ksu.edu>
2020-06-19 18:21:43 -0500

    animations

--- Assets/Scripts/DragObject.cs
+++ Assets/Scripts/DragObject.cs
@@ -12,20 +12,24 @@ public class DragObject : MonoBehaviour
 
     private void OnMouseDown()
     {
+       // print("Mouse down was called");
         mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
         mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
     }
 
     Vector3 GetMouseAsWorldPoint()
     {
-        Vector3 mousePoint = Input.mousePosition;
+        Vector3 mousePoint = new Vector3 (Input.mousePosition.x, 1.1f, Input.mousePosition.z);
         mousePoint.z = mZCoord;
+
+       // print("mouse point:" + mousePoint);
 
         return Camera.main.ScreenToWorldPoint(mousePoint);
     }
 
     private void OnMouseDrag()
     {
+       // print("mouse Dragging called");
         transform.position = GetMouseAsWorldPoint() + mOffset;
     }
 
