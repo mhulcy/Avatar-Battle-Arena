070251a6747c1a2b8c3b2283e761a998649eb1ee
JeremyBasso <jmb4433@gmail.com>
2020-06-19 13:31:25 -0500

    First Money System

---
+++ Assets/Scripts/PlayerMoney.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoney : MonoBehaviour
+{
+    public int playerMoney;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMoney = 100;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addMoney(int moneyToAdd)
+    {
+        playerMoney += moneyToAdd;
+    }
+
+    public void subtractMoney(int moneyToSubtract)
+    {
+        if(playerMoney - moneyToSubtract <= 0)
+        {
+            Debug.Log("We dont have enough money");
+        }
+        else
+        {
+            playerMoney -= moneyToSubtract;
+        }
+    }
+}
