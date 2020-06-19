2fec22dcbbf8a4cc4178053a0aac59f104d8a114
Garrett-Freddo <gafreddo@gmail.com>
2020-06-19 15:32:21 -0500

    Finished player pathfinding

---
+++ Assets/Scripts/AnimController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour
+{
+    float speed = 4;
+    float rotSpeed = 80;
+    float gravity = 8;
+    Vector3 moveDir = Vector3.zero;
+    CharacterController controller;
+    Animator animator;
+  
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    
+    void Update()
+    {
+        if (controller.isGrounded) {
+            //idk
+        }
+    }
+}
