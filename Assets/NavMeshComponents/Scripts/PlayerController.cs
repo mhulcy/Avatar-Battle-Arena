cd5946bc50d85bf380a3e5bf3846cdc62684816b
bennoishi <boishi@wustl.edu>
2020-06-19 12:27:33 -0500

    pathfinding kinda done

---
+++ Assets/NavMeshComponents/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Camera cam;
+
+
+    public NavMeshAgent agent;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                //MOVE OUR AGENT
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
