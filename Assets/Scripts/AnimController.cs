6d48dddfb1adfcbe5edbf02a016d9f32d7412f8d
mhulcy <hulcy@ksu.edu>
2020-06-19 13:42:08 -0500

    idk

--- Assets/Scripts/AnimController.cs
+++ Assets/Scripts/AnimController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class AnimController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float speed = 4;
+    float rotSpeed = 80;
+    float gravity = 8;
+    Vector3 moveDir = Vector3.zero;
+    CharacterController controller;
+    Animator animator;
+  
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (controller.isGrounded) {
+            //idk
+        }
     }
 }
