0fa8b79efb567406f40d932f46002eab0b05e4c0
JeremyBasso <jmb4433@gmail.com>
2020-06-20 12:32:52 -0500

    force commit

---
+++ Assets/Scripts/FireWarriorSpawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class FireWarriorSpawn : MonoBehaviour
+{
+    public GameObject FireWarrior;
+
+    public void spawnWarrior()
+    {
+        GameObject Fire_Warrior = Instantiate(FireWarrior, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
