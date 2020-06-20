0b2598d69327e1fe6364dea047b9787ff21b2127
JeremyBasso <jmb4433@gmail.com>
2020-06-20 18:38:35 -0500

    water spawning

---
+++ Assets/Scripts/WaterAssassinSpawn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+
+public class WaterAssassinSpawn : MonoBehaviour
+{
+    public GameObject WaterAssassin;
+
+    public void spawnWaterAssassin()
+    {
+        GameObject Water_Assassin = Instantiate(WaterAssassin, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
