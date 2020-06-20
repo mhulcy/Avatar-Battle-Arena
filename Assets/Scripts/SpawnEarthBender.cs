a19b3e50978637428bf936f2c05ea922f6d7f0b1
JeremyBasso <jmb4433@gmail.com>
2020-06-20 10:50:14 -0500

    earthSpawns

---
+++ Assets/Scripts/SpawnEarthBender.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEarthBender : MonoBehaviour
+{
+    public GameObject EarthBender;
+
+    public void spawnEarthBender()
+    {
+        GameObject Sokka = Instantiate(EarthBender, new Vector3(1, 1, 1), Quaternion.identity);
+    }
+}
