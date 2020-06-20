d3b3be8bf7a35a4a546745a42a621096b215cf8f
JeremyBasso <jmb4433@gmail.com>
2020-06-20 11:16:23 -0500

    spawn pieces

---
+++ Assets/Scripts/SpawnPieces/SpawnWaterBender.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWaterBender : MonoBehaviour
+{
+    public GameObject WaterBender;
+
+    public void spawnWaterBender()
+    {
+        GameObject Sokka = Instantiate(WaterBender, new Vector3(1, 1, 1), Quaternion.identity);
+    }
+}
