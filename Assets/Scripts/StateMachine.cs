566d514347f83cf3fb25fb9467ef413c30d6ae93
bennoishi <boishi@wustl.edu>
2020-06-20 13:16:06 -0500

    StateMachine script made

---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public enum State
+{
+    TITLE,
+    PLAYERTURN,
+    COMBAT,
+    WIN,
+    LOSE
+}
+
+public class StateMachine : MonoBehaviour
+{
+
+    public State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = State.TITLE;
+        SetupCombat();
+    }
+
+    void SetupCombat()
+    {
+
+    }
+
+}
