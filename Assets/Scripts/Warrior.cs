17eb6937e787d34360a414294e8f030f94339e9f
Garrett-Freddo <gafreddo@gmail.com>
2020-06-20 13:34:19 -0500

    pulling

--- Assets/Scripts/Warrior.cs
+++ Assets/Scripts/Warrior.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 public class Warrior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int health = 100;
+    const int range = 2;
+    int damage = 20;
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
