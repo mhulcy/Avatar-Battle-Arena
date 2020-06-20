f39576f2011e1ab80e28b31b0d7cf6f45a6019f4
mhulcy <hulcy@ksu.edu>
2020-06-20 14:16:20 -0500

    BIg Commit

--- Assets/Scripts/Board_block.cs
+++ Assets/Scripts/Board_block.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Board_block : MonoBehaviour
 {
     // Start is called before the first frame update
+    Outline outline;
     void Start()
     {
-        
+        outline = GetComponent<Outline>();
+        outline.enabled = false;
     }
 
     // Update is called once per frame
@@ -16,10 +18,11 @@ public class Board_block : MonoBehaviour
 
     }
 
-    private void OnMouseDown()
+    private void OnMouseEnter()
     {
-
-
-
+        outline.enabled = true;
+    }
+    private void OnMouseExit() {
+        outline.enabled = false;
     }
 }
