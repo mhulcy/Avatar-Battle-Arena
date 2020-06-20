9d4aafde3508a7c87be0d1d5aa589081a9bf1ad7
mhulcy <hulcy@ksu.edu>
2020-06-20 15:29:08 -0500

    as

---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public enum State
+{
+    TITLE,
+    PLAYERTURN,
+    COMBAT,
+    WIN,
+    LOSE
+}
+
+public class StateMachine : MonoBehaviour
+{
+
+    public State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = State.TITLE;
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
