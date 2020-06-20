3ac6b7b33a82b77f4688bbd5189c90d7aa39d401
mhulcy <hulcy@ksu.edu>
2020-06-20 11:08:25 -0500

    Main Menu

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PlayGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
