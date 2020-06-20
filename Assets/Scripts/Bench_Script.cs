1a971d439c66e599b7d5f840a3783e4e8e387ae6
Preston <preston.e.reed@gmail.com>
2020-06-19 22:45:41 -0500

    force commit

---
+++ Assets/Scripts/Bench_Script.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bench_Script : MonoBehaviour
+{
+    private bool isOccupied = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void occupy()
+    {
+        isOccupied = true;
+    }
+
+    void unOccupy()
+    {
+        isOccupied = false;
+    }
+
+    bool occupancy()
+    {
+        return isOccupied;
+    }
+
+
+
+}
