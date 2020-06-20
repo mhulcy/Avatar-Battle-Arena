ba21c2aef0da798d569ea0aa556b4dfb10aea43e
Garrett-Freddo <gafreddo@gmail.com>
2020-06-20 15:43:58 -0500

    pushing bug

--- Assets/Scripts/Bender.cs
+++ Assets/Scripts/Bender.cs
@@ -1,18 +1,108 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.AI;
 
 public class Bender : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int health = 60;
+    const int range = 6;
+    int damage = 30;
+    int tolerance = 5;
+    float timer = 1f;
+
+    Vector3 targetCoords = new Vector3(0, 0, 0);
+
+    Animator anim;
+    GameObject target;
+
+
+    public NavMeshAgent agent;
+
+
     void Start()
     {
-        
+       // anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        target = findEnemy();
+        if (target != null)
+        {
+            targetCoords = target.transform.position;
+           // anim.SetBool("IsWalking", true);
+            if (findDistance(this.transform.position, targetCoords) < range)
+            {
+             //   anim.SetBool("IsWalking", false);
+                agent.isStopped = true;
+                timer -= Time.deltaTime;
+                if (timer < 0)
+                {
+                    print(attack());
+                }
+            }
+            else
+            {
+                print("walking");
+                agent.isStopped = false;
+                agent.SetDestination(new Vector3 (0,0,0));
+            }
+        }
+
     }
+
+
+
+
+    int attack()
+    {
+       // anim.SetTrigger("PunchTrigger");
+        int amount;
+        print("attacks");
+        timer = 1f;
+        int addedDmg = UnityEngine.Random.Range(-5, 6);
+        amount = damage + addedDmg;
+        return amount;
+    }
+
+    GameObject findEnemy()
+    {
+        double min = 100000;
+        GameObject nearestEnemy = null;
+        double value;
+
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Enemy_Piece");
+        for (int i = 0; i < gameObjects.Length; ++i)
+        {
+            // print("found enemy");
+            value = Math.Sqrt(Math.Pow(gameObjects[i].transform.position.x - this.transform.position.x, 2) +
+                Math.Pow(gameObjects[i].transform.position.y - this.transform.position.y, 2) +
+                Math.Pow(gameObjects[i].transform.position.z - this.transform.position.z, 2));
+            if (value < min)
+            {
+                min = value;
+                nearestEnemy = gameObjects[i];
+            }
+        }
+
+        return nearestEnemy;
+    }
+
+    double findDistance(Vector3 myPos, Vector3 targetPos)
+    {
+        double distance = Math.Sqrt(Math.Pow(targetPos.x - myPos.x, 2) + Math.Pow(targetPos.z - myPos.z, 2));
+        return distance;
+
+    }
+
+
 }
