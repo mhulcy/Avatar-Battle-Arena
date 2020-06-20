265571bbfd269d2c0d711e05c3b4970a6e078bd9
JeremyBasso <jmb4433@gmail.com>
2020-06-19 23:21:38 -0500

    force commit

---
+++ Assets/Scripts/PlayerCharScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharScript : MonoBehaviour
+{
+    private int health;
+    private int damage;
+    private int attackRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+        damage = 10;
+        attackRange = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
