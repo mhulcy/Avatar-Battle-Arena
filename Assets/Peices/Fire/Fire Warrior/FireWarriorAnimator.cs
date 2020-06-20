3712d412789a5267500db5cfe547bb291f39df57
mhulcy <hulcy@ksu.edu>
2020-06-19 19:26:48 -0500

    sas

--- Assets/Peices/Fire/Fire Warrior/FireWarriorAnimator.cs
+++ Assets/Peices/Fire/Fire Warrior/FireWarriorAnimator.cs
@@ -6,26 +6,35 @@ public class FireWarriorAnimator : MonoBehaviour
 {
     float speed = 4;
 
-    Vector3 moveDir = Vector3.zero;
+    Vector3 OgPosition = Vector3.zero;
     CharacterController controller;
     Animator animator;
     Rigidbody rig;
-
+    
     void Start() {
         animator = GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
         rig = GetComponent<Rigidbody>();
+        OgPosition = this.transform.position;
     }
 
 
     void Update() {
-        Debug.Log(rig.velocity.magnitude);
-        animator.SetFloat("Speed", rig.velocity.magnitude);
-        if (rig.velocity.magnitude > 0) {
+        print(rig.velocity.magnitude);
+        //animator.SetFloat("Speed", rig.velocity.magnitude);
+        /*
+        if (OgPosition != transform.position) {
+            print("made it");
             animator.SetTrigger("Walk Forward");
+            OgPosition = transform.position;
+        }
+        */
+        if (OgPosition != this.transform.position) {
+            animator.SetBool("IsWalking", true);
+            OgPosition = transform.position;
         }
         else
-            animator.ResetTrigger("Walk Forward");
+            animator.SetTrigger("IdleNow");
     }
 }
 
