a6e3b0174b98c998661d26c6d5a6a8dabd573c03
mhulcy <hulcy@ksu.edu>
2020-06-20 10:25:49 -0500

    aw

--- Assets/Scripts/SpawnWarrior.cs
+++ Assets/Scripts/SpawnWarrior.cs
@@ -7,9 +7,10 @@ using UnityEngine;
 public class SpawnWarrior : MonoBehaviour
 {
    public GameObject EarthWarrior;
-
+      
    public void spawnWarrior()
     {
+        print("called");
         GameObject Sokka = Instantiate(EarthWarrior, new Vector3(1, 1, 1), Quaternion.identity);
     }
 
