26fb1c94f318ad2a8825d9fd4ba696397fa957db
Preston <preston.e.reed@gmail.com>
2020-06-20 21:12:01 -0500

    air slat bender

---
+++ Assets/Scripts/Tornado.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tornado : MonoBehaviour
+{
+    private GameObject target;
+    private float _speed = .2f;
+    private float _time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("ran");
+        // this.transform.position = target.transform.position;
+        this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z), _speed);
+        _time += Time.deltaTime;
+
+        if (_time > 1.5) 
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void setTarget(GameObject attackMe) 
+    {
+        target = attackMe;
+    }
+}
