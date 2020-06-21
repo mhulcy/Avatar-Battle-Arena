9ac404797806dca7a16a3009d3e74bd8c549af34
JeremyBasso <jmb4433@gmail.com>
2020-06-20 22:33:58 -0500

    force commiy

--- Assets/Scripts/EarthBender.cs
+++ Assets/Scripts/EarthBender.cs
@@ -50,12 +50,36 @@ public class EarthBender : MonoBehaviour {
                     timer -= Time.deltaTime;
                     if (timer < 0)
                     {
-                        //Water.Stop();
-                        print(attack());
-
-                        // earth.Stop();
-                        //if (projectile.transform.position == target.transform.position)
-                        //Destroy(projectile);
+                        if (target.GetComponent<Warrior_Enemy>() != null)
+                        {
+                            Warrior_Enemy instance = target.GetComponent<Warrior_Enemy>();
+                            instance.takeDamage(attack());
+                        }
+                        else if (target.GetComponent<Assasin_Enemy>() != null)
+                        {
+                            Assasin_Enemy instance = target.GetComponent<Assasin_Enemy>();
+                            instance.takeDamage(attack());
+                        }
+                        else if (target.GetComponent<FireBenderEnemy>() != null)
+                        {
+                            FireBenderEnemy instance = target.GetComponent<FireBenderEnemy>();
+                            instance.takeDamage(attack());
+                        }
+                        else if (target.GetComponent<WaterBender_Enemy>() != null)
+                        {
+                            WaterBender_Enemy instance = target.GetComponent<WaterBender_Enemy>();
+                            instance.takeDamage(attack());
+                        }
+                        else if (target.GetComponent<EarthBender_Enemy>() != null)
+                        {
+                            EarthBender_Enemy instance = target.GetComponent<EarthBender_Enemy>();
+                            instance.takeDamage(attack());
+                        }
+                        else if (target.GetComponent<Airbender_Enemy>() != null)
+                        {
+                            Airbender_Enemy instance = target.GetComponent<Airbender_Enemy>();
+                            instance.takeDamage(attack());
+                        }
 
                     }
                 }
