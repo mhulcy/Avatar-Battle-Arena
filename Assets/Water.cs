d11e2e62823495361d9c4bc6fb05ae92192c345c
mhulcy <hulcy@ksu.edu>
2020-06-20 19:54:39 -0500

    das

---
+++ Assets/Water.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    ParticleSystem WaterEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WaterEffect = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShootWater() {
+        WaterEffect.Play();
+    }
+}
