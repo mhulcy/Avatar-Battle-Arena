ad28d9a686f412137d029365b2d53da69d804597
mhulcy <hulcy@ksu.edu>
2020-07-26 14:48:10 -0500

    ok

--- Assets/Scripts/AirBender.cs
+++ Assets/Scripts/AirBender.cs
@@ -44,72 +44,69 @@ public class AirBender : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         //state = state.GetComponent<NewStateMachine>();
-
         playerControls = this.GetComponent<PlayerController>();
-
-
-        if (state.state == currentState.COMBAT)
-        {
-            print("MICHAEL");
-        }
-        
-     
-
         if (!playerControls.playerBench && combatState)
         {
-                target = findEnemy();
-                if (target != null)
+            
+            target = findEnemy();
+            if (target != null)
+            {
+                targetCoords = target.transform.position;
+                anim.SetBool("IsWalking", true);
+                if (findDistance(this.transform.position, targetCoords) < range)
                 {
-                    targetCoords = target.transform.position;
-                    anim.SetBool("IsWalking", true);
-                    if (findDistance(this.transform.position, targetCoords) < range)
+                    anim.SetBool("IsWalking", false);
+                    agent.isStopped = true;
+                    timer -= Time.deltaTime;
+                    if (timer < 0)
                     {
-                        anim.SetBool("IsWalking", false);
-                        agent.isStopped = true;
-                        timer -= Time.deltaTime;
-                        if (timer < 0)
+
+                        if (target.GetComponent<Warrior_Enemy>() != null)
                         {
-                            if (target.GetComponent<Warrior_Enemy>() != null)
-                            {
-                                Warrior_Enemy instance = target.GetComponent<Warrior_Enemy>();
-                                instance.takeDamage(attack());
-                            }
-                            else if (target.GetComponent<Assasin_Enemy>() != null)
-                            {
-                                Assasin_Enemy instance = target.GetComponent<Assasin_Enemy>();
-                                instance.takeDamage(attack());
-                            }
-                            else if (target.GetComponent<FireBenderEnemy>() != null)
-                            {
-                                FireBenderEnemy instance = target.GetComponent<FireBenderEnemy>();
-                                instance.takeDamage(attack());
-                            }
-                            else if (target.GetComponent<WaterBender_Enemy>() != null)
-                            {
-                                WaterBender_Enemy instance = target.GetComponent<WaterBender_Enemy>();
-                                instance.takeDamage(attack());
-                            }
-                            else if (target.GetComponent<EarthBender_Enemy>() != null)
-                            {
-                                EarthBender_Enemy instance = target.GetComponent<EarthBender_Enemy>();
-                                instance.takeDamage(attack());
-                            }
-                            else if (target.GetComponent<Airbender_Enemy>() != null)
-                            {
-                                Airbender_Enemy instance = target.GetComponent<Airbender_Enemy>();
-                                instance.takeDamage(attack());
-                            }
-
+                            Warrior_Enemy instance = target.GetComponent<Warrior_Enemy>();
+                            instance.takeDamage(attack());
                         }
-                    }
-                    else
-                    {
+                        else if (target.GetComponent<Assasin_Enemy>() != null)
+                        {
+                            Assasin_Enemy instance = target.GetComponent<Assasin_Enemy>();
+                            instance.takeDamage(attack());
+                        }
+                        else if (target.GetComponent<FireBenderEnemy>() != null)
+                        {
+                            FireBenderEnemy instance = target.GetComponent<FireBenderEnemy>();
+                            instance.takeDamage(attack());
+                        }
+                        else if (target.GetComponent<WaterBender_Enemy>() != null)
+                        {
+                            WaterBender_Enemy instance = target.GetComponent<WaterBender_Enemy>();
+                            instance.takeDamage(attack());
+                        }
+                        else if (target.GetComponent<EarthBender_Enemy>() != null)
+                        {
+                            EarthBender_Enemy instance = target.GetComponent<EarthBender_Enemy>();
+                            instance.takeDamage(attack());
+                        }
+                        else if (target.GetComponent<Airbender_Enemy>() != null)
+                        {
+                            Airbender_Enemy instance = target.GetComponent<Airbender_Enemy>();
+                            instance.takeDamage(attack());
+                        }
 
-                        agent.isStopped = false;
-                        agent.SetDestination(targetCoords);
+
+
+
+
+
+
                     }
-                
+                }
+                else
+                {
+                    agent.isStopped = false;
+                    agent.SetDestination(targetCoords);
+                }
             }
         }
     }
