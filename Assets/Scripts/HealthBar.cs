162f2884b6a47fdbacaf13b984e24f77de22c989
bennoishi <54510569+bennoishi@users.noreply.github.com>
2020-07-26 15:10:45 -0500

    health bar camera work

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    public void setMaxHealth(int health)
     {
-        
+        slider.maxValue = health;
+        slider.value = health; 
+
+        fill.color = gradient.Evaluate(1f);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void setHealth(int health)
     {
-        
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
+    
 }
